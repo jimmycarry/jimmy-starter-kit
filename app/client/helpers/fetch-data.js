d75d4b40c57281c104e3fdb95664fb268a15f9f0
叶志明 <yezhiming@yezhimingdeMacBook-Pro.local>
2017-03-09 22:39:09 +0800

    fixed

--- app/client/helpers/fetch-data.js
+++ app/client/helpers/fetch-data.js
@@ -25,6 +25,7 @@ export const clientFetchData = (history, routes, store) => {
         if (!isEmpty(window.prerenderData)) {
           // Delete initial data so that subsequent data fetches can occur
           window.prerenderData = undefined;
+          trigger('fetchData', renderProps.components, getLocals(store, renderProps));
         } else {
           // Fetch mandatory data dependencies for 2nd route change onwards
           trigger('fetchData', renderProps.components, getLocals(store, renderProps));
