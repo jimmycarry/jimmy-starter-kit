18141ff8b40fa9c0883778f7630d196a5bb4e534
叶志明 <yezhiming@yezhimingdeMacBook-Pro.local>
2017-03-14 09:58:07 +0800

    fixed

--- app/client/com/main/app.jsx
+++ app/client/com/main/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import 'babel-polyfill';
+import ES6 from 'es6-promise';
+
+ES6.polyfill();
 
 const App = ({ store, routes }: { store: Object, routes: Object }) => {
   if (process.env.NODE_ENV === 'development') {
